a0ddfd58badbf9e2b5d37d6e1541502fa6d5f429
Björn Petersen <nicewuerfel@gmail.com>
2016-07-13 00:14:45 +0200

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user" 2>&1
+  exit 1
+fi
+
+apt-get install libav-tools libavcodec-extra-56 libssl-dev libffi-dev libasound2-dev -y
+pip install python-telegram-bot gmusicapi youtube-dl pafy pydub simpleaudio --upgrade
