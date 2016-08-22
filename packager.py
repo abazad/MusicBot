de9a30af8c71b4b5c6601d236e5d9d8f1e277c47
Björn Petersen <nicewuerfel@gmail.com>
2016-08-22 23:20:20 +0200

    Update zip naming

--- packager.py
+++ packager.py
@@ -17,40 +17,38 @@ def _add_file(package, cur_dir, file):
     package.write(path)
 
 
-def _create_package(source_path=".", target_dir=".", branch="unknown", snapshot=True):
-    if snapshot:
-        snapshot = "-SNAPSHOT"
-    else:
-        snapshot = ""
-
-    package_fname = os.path.join(
-        target_dir, "TelegramMusicBot-{version}-{branch}{snapshot}.zip".format(version=main.__version__, branch=branch, snapshot=snapshot))
-
-    files = os.listdir(source_path)
+def _create_package():
     try:
-        with ZipFile(package_fname, 'w') as package:
-            for file in files:
-                if file in whitelist:
-                    _add_file(package, source_path, file)
-    except Exception as e:
-        print(e)
-
-if __name__ == '__main__':
-    branch = True
-    snapshot = True
-    if len(sys.argv) >= 2:
-        for arg in sys.argv[1:]:
-            if arg == "--tag" or arg == "-t":
-                branch = os.environ['CIRCLE_TAG']
-                snapshot = False
-            if arg == "--branch" or arg == "-b":
-                branch = os.environ['CIRCLE_BRANCH']
-    else:
-        print("defaulting to branch build")
+        branch = os.environ['CIRCLE_TAG']
+        tag = True
+    except KeyError:
         try:
             branch = os.environ['CIRCLE_BRANCH']
+            tag = False
         except KeyError:
-            print("not a branch build")
+            print("Neither tag nor branch build")
             sys.exit(1)
 
-    _create_package(target_dir=os.environ['CIRCLE_ARTIFACTS'], branch=branch, snapshot=snapshot)
+    repo_name = os.environ['CIRCLE_PROJECT_REPONAME']
+    target_dir = os.environ['CIRCLE_ARTIFACTS']
+    if tag:
+        sha1 = ""
+    else:
+        sha1 = "-" + os.environ['CIRCLE_SHA1']
+
+    package_fname = os.path.join(
+        target_dir, "{repo_name}-{branch}-{version}{sha1}.zip".format(repo_name=repo_name,
+                                                                      branch=branch,
+                                                                      version=main.__version__,
+                                                                      sha1=sha1))
+
+    files = os.listdir(".")
+
+    with ZipFile(package_fname, 'w') as package:
+        for file in files:
+            if file in whitelist:
+                _add_file(package, ".", file)
+
+
+if __name__ == '__main__':
+    _create_package()
