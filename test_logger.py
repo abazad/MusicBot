7aa3e0eb44ee8b094033c819ea3a37e038fa291f
Björn Petersen <nicewuerfel@gmail.com>
2016-09-06 12:30:38 +0200

    Update test_logger to just use the root logger

--- test_logger.py
+++ test_logger.py
@@ -1,11 +1,10 @@
 import logging
 import os
-import sys
 
 # Initialize logger
 os.makedirs("logs", exist_ok=True)
 
-logger = logging.getLogger("musicbot")
+logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 formatter = logging.Formatter(
@@ -14,16 +13,3 @@ formatter = logging.Formatter(
 file_handler = logging.FileHandler("logs/tests.log", encoding="utf-8", mode='w')
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
-
-info_handler = logging.StreamHandler(sys.stdout)
-info_handler.setLevel(logging.INFO)
-info_filter = logging.Filter()
-info_filter.filter = lambda record: record.levelno == logging.INFO
-info_handler.addFilter(info_filter)
-info_handler.setFormatter(formatter)
-logger.addHandler(info_handler)
-
-error_handler = logging.StreamHandler(sys.stderr)
-error_handler.setLevel(logging.ERROR)
-error_handler.setFormatter(formatter)
-logger.addHandler(error_handler)
