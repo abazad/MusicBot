3e503fe5cf93fc8e0b389720eed20641ee949679
Björn Petersen <nicewuerfel@gmail.com>
2016-10-07 09:55:16 +0200

    Replace Song.load with lambda in _try_load_song

--- musicbot/music_apis.py
+++ musicbot/music_apis.py
@@ -915,6 +915,7 @@ class OfflineAPI(AbstractSongProvider):
         except (KeyError, IndexError):
             return None
         song = Song(song_id, self, title, artist, album_art, str_rep, duration)
+        song.load = lambda: song_path
         song.loaded = True
         logging.getLogger("musicbot").debug("Loaded offline song: %s", song)
         return song
